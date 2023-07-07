2b2c4a5c76d183238650dc6763a2bbbfd1e2eaa1
Giampiero Campa <122689018+giampy1969@users.noreply.github.com>
2023-07-07 13:48:36 -0400

    Add files via upload

---
+++ contents.m
@@ -0,0 +1,30 @@
+% ATTRACTORS IN SIMULINK
+%
+% Version 2.0 (R2014b)  11-Nov-2017
+%
+% This is a collection of simulink blocks implementing simple
+% nonlinear dynamical systems that generate attractors.
+% G. Campa, June 05,  Revised March 07, Jan 17, Nov 17
+%
+%
+% MODELS:
+%
+% lorenz3d    : Lorenz system (3D, continuous time).
+% vdp2d       : Van Der Pol system (2D, continuous time).
+% henon2d     : Henon system (2D, discrete time).
+% lvpp2d      : Lotka Volterra Prey-Predator system (2D, continuous time).
+% clv1d       : Competitive Lotka Volterra system (1D, continuous time).
+% clv3d       : Competitive Lotka Volterra system (3D, continuous time).
+%
+%
+% M-FILES:
+%
+% sfun3d.m    : 3D simulink scope
+%
+%
+% OTHER FILES:
+%
+% Contents.m  : this file.
+%
+%
+% Copyright 2017, The MathWorks, Inc.
